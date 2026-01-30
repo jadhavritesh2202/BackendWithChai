744289f45e6d4dcf5387cbf7e28cd930e9353652
ritesh2004 <jadhavritesh2202@gmail.com>
2026-01-30 09:09:18 +0530

    Logout Middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -81,8 +81,114 @@ const registerUser=asyncHandler(async(req,res)=>{
      return res.status(201).json((
         new ApiResponse(200,createdUser,"User registered Successfully")
      ))
-
-
 })
 
-export {registerUser}
+//function of access and generate token
+const generateAccessAndRefreshTokens = async(userId)=>{
+   try{
+      const user=await User.findById(userId)
+     const accessToken= user.generateAccessToken()
+      const refreshToken= user.generateRefreshToken()
+      //refresh token save in dbs
+       user.refreshToken=refreshToken
+      await user.save({validateBeforeSave:false})
+
+    return {accessToken,refreshToken}
+
+   }catch(err)
+   {
+      throw new ApiError(500,"Something went wrong while generating refresh and access token")
+   }
+}
+//login user
+  const loginUser=asyncHandler(async(req,res)=>{
+          //req body--> data
+          //username or email
+          //find the user
+          //password check
+          //access and refresh token 
+          //send cookies
+
+          const {email,username,password}=req.body
+          if(!username || !email)
+          {
+            throw new ApiError(400,"username or email is requires");
+          }
+
+        const user=await User.findOne({
+            $or:[{username},{email}]
+          })
+          if(!user)
+          {
+            throw new ApiError(404,"user does not exist");
+          }
+          //check password
+         const isPasswordValid=await user.isPasswordCorrect(password)
+        if(!isPasswordValid)
+          {
+            throw new ApiError(401,"Invalid user credentails");
+          }
+       //generate access token (using function)
+         const {accessToken,refreshToken}=await generateAccessAndRefreshTokens(user._id)   
+         
+         //send cookies
+          const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+          //modify only on server
+           const options = {
+                httpOnly: true,
+                  secure: true
+         }
+         //cookies
+          return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200, 
+            {
+                user: loggedInUser, accessToken, refreshToken
+            },
+            "User logged In Successfully"
+        )
+    )
+       
+  })
+
+  //logout 
+ /* verifyJWT → req.user
+      ↓
+DB se refreshToken delete
+      ↓
+Browser se accessToken cookie delete
+      ↓
+Browser se refreshToken cookie delete
+      ↓
+User fully logged out
+*/
+  const logoutUser=asyncHandler(async(req,res)=>{
+        await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $unset: {
+                refreshToken: 1 // this removes the field from document
+            }
+        },
+        {
+            new: true
+        }
+    )
+     const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+     return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged Out"))
+
+  })
+
+export {registerUser,loginUser,logoutUser}
